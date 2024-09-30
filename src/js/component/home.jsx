a85f92e0e557d57922514706419b0b54b65baa26
Vinka Vladislavic <123342469+VinkaVladislavic@users.noreply.github.com>
2024-09-30 14:39:16 +0000

    [IMP]

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [todo, setTodo] = useState("")
+	const [tasks, setTasks] = useState([])
+	const [hidden, setHidden] = useState(null)
+
+	function keyDownFunction(e) {
+		if(todo.trim() === "") {
+			return
+		}
+		if(e.key === "Enter") {
+			setTasks([...tasks, todo.trim()])
+			setTodo("")
+		}
+	}
+
+	function deleteTask() {
+		let newTask = tasks.filter((actuaTask, i) => i !== hidden);
+		setTasks(newTask);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="todo-list-container">
+			<h1>todos</h1>
+			<div className="todo-list text-start border">
+				<div className="todo-list-input">
+					<input type="text" 
+						onChange={(e) => setTodo(e.target.value)} value={todo}
+						onKeyDown={keyDownFunction}
+						placeholder="What needs to be done?"
+						className="container-fluid"
+						/>
+				</div>
+				<ul className="list-group ">
+					{tasks.map((task, index) =>(
+						<li key={index} 
+							className="list-group-item"
+							onMouseEnter={() => {setHidden(index)}}
+							onMouseLeave={() => {setHidden(null)}}
+						>
+							{task}
+							{hidden === index ? 
+								<svg 
+									xmlns="http://www.w3.org/2000/svg" 
+									width="32" 
+									height="32" 
+									fill="currentColor" 
+									style={{ fill: "#D4ADAD" }} 
+									className="bi bi-x" 
+									viewBox="0 0 16 16"
+									onClick={deleteTask}
+								>
+									<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+								</svg> : null}
+						</li>
+					))}
+				</ul>
+				<div className="footer">
+					<p>{tasks.length === 0 ? "No tasks, add a task" : tasks.length + " item left"}</p>
+				</div>
+			</div>
+			<div className="todo-list-card-effect1 border"></div>
+			<div className="todo-list-card-effect2 border"></div>
+			<div className="todo-list-card-effect3 border"></div>
 		</div>
 	);
 };
